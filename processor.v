c7ce0b09bb52cb8f6af20bd42b87de16fd77e88b
choubobo <48757178+choubobo@users.noreply.github.com>
2019-10-29 20:45:04 -0400

    processor 10/29

--- processor.v
+++ processor.v
@@ -69,7 +69,15 @@ module processor(
     ctrl_readRegB,                  // O: Register to read from port B of regfile
     data_writeReg,                  // O: Data to write to for regfile
     data_readRegA,                  // I: Data from port A of regfile
-    data_readRegB                   // I: Data from port B of regfile
+    data_readRegB,	 // I: Data from port B of regfile
+	 q_PC_test,//////TEST
+	 alu1_out_test,
+	 Rwd0_test,
+	 Rwd1_test,
+	 mux1_out_test,mux1_in1_test,
+	 ALUinB_test,
+	 addi_test,
+	 op_code_test
 );
     // Control signals
     input clock, reset;
@@ -78,6 +86,8 @@ module processor(
     output [11:0] address_imem;
     input [31:0] q_imem;
 
+
+	 
     // Dmem
     output [11:0] address_dmem;
     output [31:0] data;
@@ -90,47 +100,65 @@ module processor(
     output [31:0] data_writeReg;
     input [31:0] data_readRegA, data_readRegB;
 
-//	module regfile(
-//	clock, ctrl_writeEnable, ctrl_reset, ctrl_writeReg,
-//	ctrl_readRegA, ctrl_readRegB, data_writeReg, data_readRegA,
-//	data_readRegB
-//);
-   // regfile my_regfile(clock, ctrl_writeEnable, ctrl_reset, ctrl_writeReg,ctrl_readRegA, ctrl_readRegB, data_writeReg, data_readRegA,data_readRegB);
 
-wire [4:0]op_code = q_imem[31:27];
-wire [4:0]r_rd = q_imem[26:22];//R type, I type, JII type
-wire [4:0]r_rs = q_imem[21:17];//R type, I type
+wire [4:0]op_code;
+assign op_code = q_imem[31:27];
 
-//R type
-wire [4:0]r_rt = q_imem[16:12];
-wire [4:0]shamt = q_imem[11:7];
-wire [4:0]ALUOp = q_imem[6:2];
 
-//I type
-wire [16:0]imme = q_imem[16:0];
+//////TEST
+output [4:0]op_code_test;
+assign op_code_test=op_code;
 
-//JI type
-wire [26:0]target = q_imem[26:0];
 
+
+
+wire [4:0]mux9_out,mux10_out;//R type, I type, JII type
+wire [4:0]r_rt; 
+wire [4:0]shamt;
+assign shamt = q_imem[11:7];
+wire [4:0]ALUOp;
+assign ALUOp = q_imem[6:2];
+
+wire [16:0]imme;
+assign imme = q_imem[16:0];
+wire [26:0]target;
+assign target = q_imem[26:0];
+wire [31:0]mux1_in1;
+wire [31:0] mux1_out,alu1_out;
+wire [31:0]q_PC,mux8_out,alu2_out,alu3_out;
+wire isNotEqual1, isLessThan1, overflow1,isNotEqual2, isLessThan2, overflow2,isNotEqual3, isLessThan3, overflow3;
+wire [31:0]mux2_out,mux3_out,mux4_out,mux5_out,mux6_out,mux7_out,mux11_out,mux12_out,mux13_out,mux14_out; 
+wire r_type,addi,sw,lw,j,bne,jal,jr,blt,bex,setx,add,sub;	
+wire and1_out,and2_out,or1_out;
+wire BR1,JP1,JP0,ALUinB,ctrl_writeEnable,Rwd1,Rwd0,BR2,r30_or_r31,mux10_control,rstatus_1_or_2,rstatus_3_or_T,mux13_control,mux14_control;
+
+
+assign mux9_out = r30_or_r31? 5'b11111:5'b11110;
+
+assign r_rt = bex? 5'b00000 : q_imem[16:12];//bex = 1, rt choose r0
+assign ctrl_writeReg = mux10_control? q_imem[26:22] : mux9_out;
+assign ctrl_readRegA = bex? 5'b11110 : q_imem[21:17];//bex = 1, rs choose r30
+assign ctrl_readRegB = sw ? q_imem[26:22] : r_rt;
 //sign-extend immediate
-wire [31:0]mux1_in1 = {{15'b0},imme};
+
+assign mux1_in1=imme[16] ? {{15{1'b1}},imme} :{{15'b0},imme};//15‘b1:用15个bit表示1
+
+
 
 //module mux(select, in0, in1, out);
-mux
-   mux1(ALUinB,data_readRegB,mux1_in1,mux1_out);
+mux mux1(ALUinB,data_readRegB,mux1_in1,mux1_out);
 	
 //module alu(data_operandA, data_operandB, ctrl_ALUopcode,
 	//		ctrl_shiftamt, data_result, isNotEqual, isLessThan, overflow);
-wire [31:0] mux1_out,alu1_out;
-alu
-   alu1(data_readRegA,mux1_out,ALUOp,shamt,alu1_out,isNotEqual1, isLessThan1, overflow1);
-	
-//
-//    // Dmem
-//    output [11:0] address_dmem;
-//    output [31:0] data;
-//    output wren;
-//    input [31:0] q_dmem;
+
+	////TEST
+
+alu alu1(data_readRegA,mux1_out,ALUOp,shamt,alu1_out,isNotEqual1, isLessThan1, overflow1);
+
+output [31:0]alu1_out_test;
+assign alu1_out_test = alu1_out; 
+//bex
+
 
 assign address_dmem=alu1_out[11:0];
 assign data=data_readRegB;
@@ -139,23 +167,33 @@ mux
    mux2(Rwd0,q_dmem,q_dmem,mux2_out);
 
 //module PC(q,d,clk,en,clr);
+output [31:0]q_PC_test;
+assign q_PC_test = q_PC;
 
-wire [31:0]q_PC,mux8_out,alu2_out,alu3_out;
 PC
   PC(q_PC,mux8_out,clock,1'b1,reset);
 
-
+assign address_imem = q_PC[11:0];
 alu
-   alu2(q_PC,32'b00000000000000000000000000000001,5'b00000,5'b00000,alu2_out,isNotEqual2, isLessThan2, overflow2);
+   alu2(q_PC,32'd1,5'b0,5'b0,alu2_out,isNotEqual2, isLessThan2, overflow2);
  
+
+
 mux
    mux3(Rwd0,alu1_out,alu2_out,mux3_out),
-   mux4(Rwd1,mux3_out,mux2_out,mux4_out);
+   mux4(Rwd1,mux3_out,mux2_out,mux4_out),
+   mux11(rstatus_1_or_2,32'd1,32'd2,mux11_out),
+	mux12(rstatus_3_or_T,32'd3,{{5'b0},target},mux12_out),
+	mux13(mux13_control,mux11_out,mux12_out,mux13_out),
+	mux14(mux14_control,mux4_out,mux13_out,mux14_out);
+	
+assign data_writeReg=mux14_out;
 
-assign data_writeReg=mux4_out;
-
+//module alu(data_operandA, data_operandB, ctrl_ALUopcode,
+	//		ctrl_shiftamt, data_result, isNotEqual, isLessThan, overflow);
 alu
    alu3(alu2_out,mux1_in1,5'b00000,5'b00000,alu3_out,isNotEqual3, isLessThan3, overflow3);
+
 
 and and1(and1_out,isNotEqual1,BR1);
 and and2(and2_out,isLessThan1,BR2);
@@ -164,12 +202,13 @@ or or1(or1_out,and1_out,and2_out);
 mux
    mux5(or1_out,alu2_out,alu3_out,mux5_out),
 	mux6(JP0,mux5_out,data_readRegA,mux6_out),
-	mux7(JP0,target,target,mux7_out),
+	mux7(JP0,{{5'b0},target},{{5'b0},target},mux7_out),
 	mux8(JP1,mux6_out,mux7_out,mux8_out);
 
+
 //opcode
-assign r_type =(~op_code[4]&~op_code[3]&~op_code[2]&~op_code[1]&~op_code[0]);//add:00000
-assign addi = (~op_code[4]&~op_code[3]&op_code[2]&~op_code[1]&op_code[0]);//addi:00101
+assign r_type =((~op_code[4])&(~op_code[3])&(~op_code[2])&(~op_code[1])&~op_code[0]);//add:00000
+assign addi = ((~op_code[4])&(~op_code[3])&(op_code[2])&(~op_code[1])&(op_code[0]));//addi:00101
 assign sw = (~op_code[4]&~op_code[3]&op_code[2]&op_code[1]&op_code[0]);//sw:00111
 assign lw = (~op_code[4]&op_code[3]&~op_code[2]&~op_code[1]&~op_code[0]);//lw:01000
 assign j= (~op_code[4]&~op_code[3]&~op_code[2]&~op_code[1]&op_code[0]);//j:00001
@@ -179,14 +218,39 @@ assign jr = (~op_code[4]&~op_code[3]&op_code[2]&~op_code[1]&~op_code[0]);//jr:00
 assign blt = (~op_code[4]&~op_code[3]&op_code[2]&op_code[1]&~op_code[0]);//blt:00110
 assign bex=(op_code[4]&~op_code[3]&op_code[2]&op_code[1]&~op_code[0]);//bex:10110
 assign setx = (op_code[4]&~op_code[3]&op_code[2]&~op_code[1]&op_code[0]);//setx:10101
+assign add = (~ALUOp[4]&~ALUOp[3]&~ALUOp[2]&~ALUOp[1]&~ALUOp[0]&~op_code[4]&~op_code[3]&~op_code[2]&~op_code[1]&~op_code[0]); //00000
+assign sub = (~ALUOp[4]&~ALUOp[3]&~ALUOp[2]&~ALUOp[1]&ALUOp[0]&~op_code[4]&~op_code[3]&~op_code[2]&~op_code[1]&~op_code[0]);//00001
 
-assign BR1=bne?1:0;
-assign JP1 = j | jal ? 1 : 0;
-assign JP0 = jr ? 1 :0;
-assign ALUinB = addi | sw | lw ? 1 :0;
-assign wren = sw ? 1 : 0;//write enable for dmem
-assign ctrl_writeEnable = r_type|addi|lw|jal ?1:0;
-assign Rwd1=lw?1:0;
-assign Rwd0 = jal?1:0;
-assign BR2 = blt?1:0;
+
+assign BR1=bne?1'b1:1'b0;
+assign JP1 = j | jal|(bex&isNotEqual1) ? 1'b1 : 1'b0;//
+assign JP0 = jr ? 1'b1:1'b0;
+assign ALUinB = addi | sw | lw ? 1'b1:1'b0;
+assign wren = sw ? 1'b1:1'b0;//write enable for dmem
+assign ctrl_writeEnable = r_type|addi|lw|jal ?1'b1:1'b0;
+assign Rwd1=lw?1'b1:1'b0;
+assign Rwd0 = jal?1'b1:1'b0;
+assign BR2 = blt?1'b1:1'b0;
+assign r30_or_r31 = jal ?1'b1:1'b0;
+assign mux10_control = ~((overflow1&(add|sub|addi))|setx|jal)? 1'b1:1'b0;
+assign rstatus_1_or_2 = addi ?1'b1:1'b0;
+assign rstatus_3_or_T = setx? 1'b1:1'b0;
+assign mux13_control = (sub|setx) ? 1'b1:1'b0;
+assign mux14_control = ((add&overflow1)|setx|(sub&overflow1)|(addi&overflow1))?1'b1:1'b0;
+
+
+///////TEST
+output Rwd1_test,addi_test;
+assign Rwd1_test= Rwd1;
+assign addi_test= addi;
+
+output Rwd0_test;
+assign Rwd0_test= Rwd0;
+
+//////////////TEST
+output [31:0] mux1_out_test,mux1_in1_test;
+assign mux1_out_test = mux1_out;
+assign mux1_in1_test = mux1_in1;
+output ALUinB_test; 
+assign ALUinB_test= ALUinB;
 endmodule
--- skeleton.v
+++ skeleton.v
@@ -9,7 +9,27 @@
  * inspect which signals the processor tries to assert when.
  */
 
-module skeleton(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_clock);
+module skeleton(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_clock,reg0,reg1,reg2,reg3,
+	reg4,
+	reg5,
+	reg6,
+	reg7,
+	
+	
+	
+	 q_PC_test,//////TEST
+	 alu1_out_test,
+	 Rwd0_test,
+	 Rwd1_test,
+	 mux1_out_test,mux1_in1_test,
+	 ALUinB_test,addi_test,op_code_test,
+	 address_imem_test,q_imem_test,
+	 address_dmem_test,wren_test,q_dmem_test	
+	 );
+	 
+	 
+	 
+	 
     input clock, reset;
     output imem_clock, dmem_clock, processor_clock, regfile_clock;
 
@@ -18,12 +38,32 @@ module skeleton(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_c
     // Make sure you configure it correctly!
     wire [11:0] address_imem;
     wire [31:0] q_imem;
-    imem my_imem(
+	 
+	   imem my_imem(
         .address    (address_imem),            // address of data
-        .clock      (~imem_clock),                  // you may need to invert the clock
+        .clock      (clock),                  // you may need to invert the clock
         .q          (q_imem)                   // the raw instruction
     );
 
+	 
+	 
+	  //////TEST
+	 output [11:0]address_imem_test;
+	 assign address_imem_test = address_imem;
+	 output [31:0] q_imem_test;
+	 
+	 assign q_imem_test=q_imem;//////skeleton 的 q_imem
+	
+output [11:0] address_dmem_test;
+assign address_dmem_test= address_dmem;
+output wren_test;
+assign wren_test = wren;
+output [31:0] q_dmem_test;
+
+assign q_dmem_test = q_dmem;
+	
+	 
+	 
     /** DMEM **/
     // Figure out how to generate a Quartus syncram component and commit the generated verilog file.
     // Make sure you configure it correctly!
@@ -32,11 +72,11 @@ module skeleton(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_c
     wire wren;
     wire [31:0] q_dmem;
     dmem my_dmem(
-        .address    (/* 12-bit wire */),       // address of data
-        .clock      (~dmem_clock),                  // may need to invert the clock
-        .data	    (/* 32-bit data in */),    // data you want to write
-        .wren	    (/* 1-bit signal */),      // write enable
-        .q          (/* 32-bit data out */)    // data from dmem
+        .address    (address_dmem),       // address of data
+        .clock      (~clock),                  // may need to invert the clock
+        .data	    (data),    // data you want to write
+        .wren	    (wren),      // write enable
+        .q          (q_dmem)    // data from dmem
     );
 
     /** REGFILE **/
@@ -45,8 +85,20 @@ module skeleton(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_c
     wire [4:0] ctrl_writeReg, ctrl_readRegA, ctrl_readRegB;
     wire [31:0] data_writeReg;
     wire [31:0] data_readRegA, data_readRegB;
+	 
+	 //////TEST
+	 output [31:0]reg0,reg1,reg2,reg3,
+	reg4,
+	reg5,
+	reg6,
+	reg7;
+
+
+
+	
+	 
     regfile my_regfile(
-        regfile_clock,
+        clock,
         ctrl_writeEnable,
         reset,
         ctrl_writeReg,
@@ -54,33 +106,58 @@ module skeleton(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_c
         ctrl_readRegB,
         data_writeReg,
         data_readRegA,
-        data_readRegB
+        data_readRegB,
+		  reg0, ////////test
+		  reg1,
+		  reg2,
+		  reg3,
+	reg4,
+	reg5,
+	reg6,
+	reg7
     );
-
+	 
+	
+//////TEST
+output [31:0] q_PC_test,alu1_out_test,mux1_out_test,mux1_in1_test;//////TEST
+output Rwd0_test,Rwd1_test,ALUinB_test,addi_test;
+output [4:0]op_code_test;	 
+	 
     /** PROCESSOR **/
     processor my_processor(
         // Control signals
-        processor_clock,                          // I: The master clock
-        reset,                          // I: A reset signal
+        .clock(clock),                          // I: The master clock
+        .reset(reset),                          // I: A reset signal
 
         // Imem
-        address_imem,                   // O: The address of the data to get from imem
-        q_imem,                         // I: The data from imem
+        .address_imem(address_imem),                   // O: The address of the data to get from imem
+        .q_imem(q_imem),                         // I: The data from imem
 
         // Dmem
-        address_dmem,                   // O: The address of the data to get or put from/to dmem
-        data,                           // O: The data to write to dmem
-        wren,                           // O: Write enable for dmem
-        q_dmem,                         // I: The data from dmem
+        .address_dmem(address_dmem),                   // O: The address of the data to get or put from/to dmem
+        .data(data),                           // O: The data to write to dmem
+        .wren(wren),                           // O: Write enable for dmem
+        .q_dmem(q_dmem),                         // I: The data from dmem
 
         // Regfile
-        ctrl_writeEnable,               // O: Write enable for regfile
-        ctrl_writeReg,                  // O: Register to write to in regfile
-        ctrl_readRegA,                  // O: Register to read from port A of regfile
-        ctrl_readRegB,                  // O: Register to read from port B of regfile
-        data_writeReg,                  // O: Data to write to for regfile
-        data_readRegA,                  // I: Data from port A of regfile
-        data_readRegB                   // I: Data from port B of regfile
+        .ctrl_writeEnable(ctrl_writeEnable),               // O: Write enable for regfile
+        .ctrl_writeReg(ctrl_writeReg),                  // O: Register to write to in regfile
+        .ctrl_readRegA(ctrl_readRegA),                  // O: Register to read from port A of regfile
+        .ctrl_readRegB(ctrl_readRegB),                  // O: Register to read from port B of regfile
+        .data_writeReg(data_writeReg),                  // O: Data to write to for regfile
+        .data_readRegA(data_readRegA),                  // I: Data from port A of regfile
+        .data_readRegB(data_readRegB),                   // I: Data from port B of regfile
+		  
+		 
+		 .q_PC_test(q_PC_test),//////TEST
+	 .alu1_out_test(alu1_out_test),
+	 .Rwd0_test(Rwd0_test),
+	 .Rwd1_test(Rwd1_test),
+	 .mux1_out_test(mux1_out_test),
+	 .mux1_in1_test(mux1_in1_test),
+	 .ALUinB_test(ALUinB_test),
+	 .addi_test(addi_test),
+	 .op_code_test(op_code_test)
     );
 
 endmodule
